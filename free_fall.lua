22ec7640a75044a421050678caf769b82d0d5df2
Cvar1984 <cvar1984@cvar1984.my.id>
2024-09-03 17:09:10 +0700

    space object introduced

--- free_fall.lua
+++ free_fall.lua
@@ -1,14 +1,22 @@
-local initial_velocity, time, gravity, height;
+local gravity_tables = {
+    ["moon"] = 1.625,
+    ["earth"] = 9.80665,
+    ["mars"] = 3.72076,
+    ["sun"] = 274,
+    ["betelgeus"] = 0.003162,
+}
 
--- The gravitational acceleration of the Earth is 9.80665 m/s².
-gravity = 9.80665;
+io.write("Select space object[earth,moon,mars,sun,betelgeus]: ")
+local choice = io.read("*l")
+choice = string.lower(choice)
+local gravity = gravity_tables[choice]
 
-io.write("Initial acceleration of the object in m/s: ");
-initial_velocity = io.read("*n");
+io.write("Initial acceleration of the object in m/s: ")
+local initial_velocity = io.read("*n")
 
-io.write("Times the object is dropped from a height in sec: ");
-time = io.read("*n");
+io.write("Times the object is dropped from a height in sec: ")
+local time = io.read("*n");
 
-height = initial_velocity * time + 0.5 * gravity * time ^ 2;
+local height = initial_velocity * time + 0.5 * gravity * time ^ 2
 
-print(string.format("Your height: %s Meter", height));
+print(string.format("Your height: %s Meter", height))

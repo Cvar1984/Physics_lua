7828f736847d74e822e377d7a494014a2c3bd6c6
Cvar1984 <cvar1984@cvar1984.my.id>
2024-10-17 18:00:01 +0700

    refactor

--- telescope.lua
+++ telescope.lua
@@ -16,59 +16,50 @@ function Telescope:calculateFocalRatio(focalLength, diameter)
     return focalLength / diameter
 end
 
-function Telescope:calculateMagnification(eyepieceFocalLength)
-    return self.focalLength / eyepieceFocalLength
+function Telescope:calculateMagnification(focalLength,eyepieceFocalLength)
+    return focalLength / eyepieceFocalLength
 end
 
-function Telescope:calculateAperture(focalRatio)
-    return self.focalLength / focalRatio
+function Telescope:calculateAperture(focalLength, focalRatio)
+    return focalLength / focalRatio
 end
 -- LGP = π * (Aperture in meter/2)^2 * Luminance Sensitivity of the Human Eye in cd/m^2
-function Telescope:calculateLightGatheringPower()
+function Telescope:calculateLightGatheringPower(aperture) -- according to llama-3
     -- Luminance sensitivity of the human eye in candela per square meter approximately 3.18309 * 10^-6 cd/m^2 according to Lamma-3 AI
     -- and approximately peak at 5.5 x 10^-10 cd/m^2 according to Gemini AI
     local luminanceSensitivity = 3.18309 * 1e-6
-    local apertureInMeters = self.aperture / 1000 -- Convert mm to meters
+    local apertureInMeters = aperture / 1000 -- Convert mm to meters
 
     local lightGatheringPower = math.pi * (apertureInMeters / 2)^2 * luminanceSensitivity
 
     return lightGatheringPower
 end
 
--- Get user input
-print("Enter the diameter of the primary mirror in millimeters:")
-local diameter = tonumber(io.read())
+--local telescope = Telescope:new{}
+print("Main mirror diameter in mm: ")
+local mainMirrorDiameter = tonumber(io.read())
 
-print("Enter the focal ratio of the telescope:")
-local focalRatio = tonumber(io.read())
+print("Main mirror focal length in mm: ")
+local mainMirrorFocalLength = tonumber(io.read())
 
-print("Enter the focal length of the eyepiece in millimeters:")
-local eyepieceFocalLength = tonumber(io.read())
+local mainMirrorFocalRatio = Telescope:calculateFocalRatio(mainMirrorFocalLength, mainMirrorDiameter)
 
--- Create a new Telescope object
-local telescope = Telescope:new{
-    diameter = diameter,
-    focalRatio = focalRatio,
-}
+print("Eyepiece 1 focal length in mm: ")
+local eyepieceFocalLength1 = tonumber(io.read())
 
--- Calculate the focal length of the telescope
-telescope.focalLength = telescope:calculateFocalLength(telescope.diameter, telescope.focalRatio)
+print("Eyepiece 2 focal length in mm: ")
+local eyepieceFocalLength2 = tonumber(io.read())
 
--- Calculate the focal ratio of the telescope
-telescope.focalRatio = telescope:calculateFocalRatio(telescope.focalLength, telescope.diameter)
+local focalRatio = Telescope:calculateFocalRatio(mainMirrorFocalLength, mainMirrorDiameter)
+local magnification1 = Telescope:calculateMagnification(mainMirrorFocalLength, eyepieceFocalLength1)
+local magnification2 = Telescope:calculateMagnification(mainMirrorFocalLength, eyepieceFocalLength2)
+local aperture = Telescope:calculateAperture(mainMirrorFocalLength, mainMirrorFocalRatio)
+local apertureInches = aperture / 25.4
+local lightGatheringPower = Telescope:calculateLightGatheringPower(aperture)
 
--- Calculate the magnification of the telescope
-telescope.magnification = telescope:calculateMagnification(eyepieceFocalLength)
-
--- Calculate the aperture of the telescope
-telescope.aperture = telescope:calculateAperture(telescope.focalRatio)
-
--- Calculate the light-gathering power of the telescope
-telescope.lightGatheringPower = telescope:calculateLightGatheringPower()
-
--- Print the results
-print("Focal Length: ".. telescope.focalLength.. " mm")
-print("Focal Ratio: ".. telescope.focalRatio)
-print("Magnification: ".. telescope.magnification)
-print("Aperture: ".. telescope.aperture.. " mm")
-print("Light-Gathering Power: ".. telescope.lightGatheringPower.. " lm/s")
+print("Focal length: ".. mainMirrorFocalLength.. " mm")
+print("Focal ratio: f/".. focalRatio)
+print("Magnification 1: ".. magnification1.."X")
+print("Magnification 2: ".. magnification2.."X")
+print("Aperture: ".. aperture.. " mm (\""..apertureInches..")")
+print("Light Gathering power: ".. lightGatheringPower.. " lm/s")

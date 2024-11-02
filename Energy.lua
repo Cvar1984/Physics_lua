5362f3e430b46e85825fbe32893f98e876970729
Cvar1984 <cvar1984@cvar1984.my.id>
2024-11-02 17:35:53 +0700

    Heat energy

--- Energy.lua
+++ Energy.lua
@@ -1,30 +1,47 @@
 -- TODO: kinetic, potential, sound, mechanical, elastic, radiant, chemical, electric, nuclear, heat
-local Energy = {
-    speedOfLight = 299792458, -- m/s^2
-    gravity = 9.80665 -- m/s^2
+local Energy = { -- energy constant
+    SPEED_OF_LIGHT = 299792458, -- m/s^2
+    GRAVITY = 9.80665 -- m/s^2
 }
 
--- mas in gram, velocity in meter/second
+-- E = mc^2
+-- mass in kilograms
+function Energy:calculateEnergy(mass)
+    local energy = mass * self.SPEED_OF_LIGHT ^ 2
+    return energy
+end
+
+-- KE = 1/2mv^2
+-- mas in kilograms, velocity in meter/second
 function Energy:calculateKineticEnergy(mass, velocity)
-    mass = mass / 1000 -- Convert mass from grams to kilograms
     local kineticEnergy = 1/2 * mass * velocity ^ 2
     return kineticEnergy
 end
 
--- mas in grams, velocity in meter/second, height in meter
+-- PE = mgh
+-- mas in kilograms, height in meter
 function Energy:calculatePotentialEnergy(mass, height)
-    mass = mass / 1000 -- Convert mass from grams to kilograms
-    local potentialEnergy = mass * self.gravity * height
+    local potentialEnergy = mass * self.GRAVITY * height
     return potentialEnergy
 end
--- mass in grams
-function Energy:calculateEnergy(mass)
-    mass = mass / 1000 -- grams to kilograms
-    local energy = mass * self.speedOfLight ^ 2
-    return energy
+
+-- q = mCΔT
+-- mass in kilograms, heatcap j/kg celcius, tempchange in celcius
+function Energy:calculateHeatEnergy(mass, heatCapacity, tempChanges)
+    local heatEnergy = mass * heatCapacity * tempChanges
+    return heatEnergy
 end
 
--- test
-print("E = " .. Energy:calculateEnergy(1) .. " J")
-print("KE = " .. Energy:calculateKineticEnergy(3000, 100 * 3.6) .. " J") -- ms to kmh
-print("PE = " .. Energy:calculatePotentialEnergy(3000, 100) .. " J")
+-- C = q/(mΔT)
+-- heat energy in joules, mass in kg, tempchange in celcius
+function Energy:calculateHeatCapacity(heatEnergy, mass, tempChanges)
+    local heatCapacity = heatEnergy / (mass * tempChanges)
+    return heatCapacity
+end
+
+-- annihilation of 1 gram of matter and 1 gram of antimatter is aproximately 21.5 kilotons of TNT explosive about the same as the fat man
+print("E = " .. Energy:calculateEnergy(1/1000) .. " J")
+print("KE = " .. Energy:calculateKineticEnergy(3, 100 * 3.6) .. " J") -- ms to kmh
+print("PE = " .. Energy:calculatePotentialEnergy(3, 10) .. " J")
+print("q = " .. Energy:calculateHeatEnergy(3, 4.2, 10) .. " J")
+print("C = " .. Energy:calculateHeatCapacity(126, 3, 10) .. " J/(kg°C)")

3e3ce7970d9e654c95aa12606fe584912a91df5d
Cvar1984 <cvar1984@cvar1984.my.id>
2024-11-19 15:31:03 +0700

    missing ^

--- ShapeConversion.lua
+++ ShapeConversion.lua
@@ -1,8 +1,8 @@
+local math = require "math"
 local Math = require "Math"
-Math = Math:new()
 
-local Shape =  {}
-function Shape:new(o)
+local ShapeConversion =  {}
+function ShapeConversion:new(o)
     o = o or {}
     setmetatable(o, self)
     self.__index = self
@@ -13,16 +13,11 @@ pi = c/d
 c = pi*d
 d = c/pi
 ]]--
-function Shape:squareToCircle(squareArea) -- eg: 28m^2
-    local circleRadius = Math:sqrt(squareArea / math.pi) -- r= √(a^2 / π)
+function ShapeConversion:squareToCircle(squareArea) -- eg: 28m^2
+    local circleRadius = Math:sqrt(squareArea ^ 2 / math.pi) -- r= √(a^2 / π)
     --local circleDiameter = 2 * circleRadius -- d = 2r
     --local circleCircumference = 2 * math.pi * circleRadius-- c = 2πR
     --local circlePI = circleCircumference / circleDiameter -- π = circumference / diameter
     return circleRadius
 end
-
-print(Shape:squareToCircle(28))
--- a 28m^2
--- r 2.9854106607209
--- c 18.757888399339
--- d 5.9708213214418
+return ShapeConversion

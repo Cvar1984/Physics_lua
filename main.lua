bf06a221ae738cb2684fc815257395e7bc45a610
Cvar1984 <cvar1984@cvar1984.my.id>
2024-11-20 16:31:10 +0700

    update

--- main.lua
+++ main.lua
@@ -2,9 +2,12 @@ local Math = require "Math"
 local Energy = require "Energy"
 local Shape = require "ShapeConversion"
 
--- annihilation of 1 gram of matter and 1 gram of antimatter is aproximately 21.5 kilotons of TNT explosive about the same as the fat man
-print("E = " .. Energy:calculateRelativeEnergy(1/1000) .. " J")
-print("F = " .. Energy:calculateForce(1/1000, Math.SPEED_OF_LIGHT) .. " N")
+-- 1 gram of weights
+local mass = Energy:calculateMass(1/1000, Math.GRAVITY)
+local momentum = Energy:calculateMomentum(mass, Math.GRAVITY)
+
+print("E = " .. Energy:calculateRelativeEnergy(mass, momentum) .. " J")
+print("F = " .. Energy:calculateForce(mass, Math.SPEED_OF_LIGHT) .. " N")
 print("KE = " .. Energy:calculateKineticEnergy(3, 100 * 3.6) .. " J") -- ms to kmh
 print("PE = " .. Energy:calculatePotentialEnergy(3, 10) .. " J")
 print("q = " .. Energy:calculateHeatEnergy(3, 4.2, 10) .. " J")

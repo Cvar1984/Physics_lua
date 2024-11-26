b1a68f6922fcb1a514a6dffdbec66b0f3f7a8806
Cvar1984 <cvar1984@cvar1984.my.id>
2024-11-26 13:00:51 +0700

    format

--- main.lua
+++ main.lua
@@ -6,7 +6,7 @@ local Astro = require "Astro"
 local mass = Energy:calculateMass(1/1000, Math.GRAVITY)
 local momentum = Energy:calculateMomentum(mass, Math.GRAVITY)
 
-print("E = " .. Energy:calculateRelativeEnergy(mass, momentum) .. " J")
+print("E = " .. Math:formatNumber(Energy:calculateRelativeEnergy(mass, momentum)) .. " J")
 print("F = " .. Energy:calculateForce(mass, Math.SPEED_OF_LIGHT) .. " N")
 print("KE = " .. Energy:calculateKineticEnergy(3, 100 * 3.6) .. " J") -- ms to kmh
 print("PE = " .. Energy:calculatePotentialEnergy(3, 10) .. " J")
@@ -17,6 +17,6 @@ print("R = " .. Energy:calculateResistance(5, 50) .. " Ohm")
 print("P = " .. Energy:calculatePower(5,50) .. " W")
 print("V = " .. Energy:calculateVoltage(50, 0.1) .. " V")
 print("v = " .. Astro:calculateDopplerRadialVelocity(434, 435) .. " m/s")
-local shift = Astro:calculateDopplerShift(434/(1*10^9), 435/(1*10^9)) -- 1e+9 nm to m conversion
+local shift = Astro:calculateDopplerShift(434, 435)
 print("z = " .. shift, shift * 100 .. "%") -- redshift
-print("d = " .. Astro:calculateDistance(shift), "Light Years")
+print("d = " .. Math:formatNumber(Astro:calculateDopplerDistance(shift)), "Light Years")

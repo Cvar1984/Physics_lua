04d395136215d10d197bf89ac53957629708f208
Cvar1984 <cvar1984@cvar1984.my.id>
2024-12-17 18:03:47 +0700

    to benchmark math method

---
+++ Benchmark.lua
@@ -0,0 +1,43 @@
+---@diagnostic disable: need-check-nil
+local Math = require "Math"
+local Benchmark = {}
+
+setmetatable(Benchmark, { __index = Benchmark })
+
+function Benchmark:calculatePercentage(fasterTime, slowerTime)
+    local delta = slowerTime - fasterTime
+    return delta / slowerTime * 100
+end
+
+function Benchmark:functions(callback, iteration)
+    iteration = iteration or 1
+    local totalTime = 0
+
+    for i = 0, iteration do
+        local initialTime = os.clock()
+        callback()
+        local finalTime = os.clock()
+        local deltaTime = finalTime - initialTime
+        totalTime = totalTime + deltaTime
+    end
+
+    local averageTime = totalTime / iteration
+    return averageTime
+end
+
+--return Benchmark
+local f1 = Benchmark:functions(
+    function()
+        Math:pi(17)
+    end, 10)
+
+local f2 = Benchmark:functions(
+    function()
+        Math:pi(2)
+    end, 10)
+
+local fasterTime = math.min(f1, f2)
+local slowerTime = math.max(f1, f2)
+
+local avgT = Benchmark:calculatePercentage(fasterTime, slowerTime)
+print(avgT .. "%")

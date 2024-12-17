a3ed2af5397320ea29f6070780953863f1d017e6
Cvar1984 <cvar1984@cvar1984.my.id>
2024-12-17 15:05:34 +0700

    pi limit 17

--- Fourier.lua
+++ Fourier.lua
@@ -8,8 +8,8 @@ function Fourier:transformSawtooth(i, f, t)
     local term = 0
 
     for n = 1, i do
-        term = term + (-1 ^ n * math.sin(2 * Math:pi(19) * n * f * t) / n)
+        term = term + (-1 ^ n * math.sin(2 * Math:pi(17) * n * f * t) / n)
     end
-    return 2 / Math:pi(19) * term
+    return 2 / Math:pi(17) * term
 end
 return Fourier

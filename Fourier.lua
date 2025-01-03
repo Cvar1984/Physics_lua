13e5f8fc7d866a81cdb36abfe25c4859316f4aa6
Cvar1984 <cvar1984@cvar1984.my.id>
2025-01-03 16:14:58 +0700

    reconstruct sawtooth

--- Fourier.lua
+++ Fourier.lua
@@ -1,7 +1,7 @@
 local Math = require "Math"
 local Fourier = {}
 
-setmetatable(Fourier, {__index = Fourier})
+setmetatable(Fourier, { __index = Fourier })
 
 ---CoefficientsWave
 ---@param i number index
@@ -19,21 +19,19 @@ end
 ---@param t number time
 ---@return number result sinwave
 function Fourier:sine(i, f, t)
-    -- i is the index, f is the frequency, t is the time
     return math.sin(2 * Math:pi() * f * i * t)
 end
 
 ---Recompose signals
----@param coeffs table coefficients signals
+---@param coeffs table sin & cosin coefficients signals
 ---@param f number frequency
 ---@param t number time
 ---@return number result composed signals
 function Fourier:recompose(coeffs, f, t)
-    -- coeffs is a table of the Fourier series coefficients (A_n, B_n)
     local result = 0
     for n, coeff in ipairs(coeffs) do
         -- Rebuild signal using both sine and cosine terms
-        result = result + coeff.A * self:cosine(n, f, t) + coeff.B * self:sine(n, f, t)
+        result = result + coeff.cos * self:cosine(n, f, t) + coeff.sin * self:sine(n, f, t)
     end
     return result
 end
@@ -48,18 +46,18 @@ function Fourier:decompose(signal, maxHarmonic, f, t)
     local coeffs = {}
 
     for n = 1, maxHarmonic do
-        -- Calculate A_n (cosine coefficient) and B_n (sine coefficient)
-        local A_n, B_n = 0, 0
+        local cosC, sinC = 0, 0
+
         for i, ti in ipairs(t) do
-            A_n = A_n + signal[i] * self:cosine(n, f, ti)
-            B_n = B_n + signal[i] * self:sine(n, f, ti)
+            cosC = cosC + signal[i] * self:cosine(n, f, ti)
+            sinC = sinC + signal[i] * self:sine(n, f, ti)
         end
         -- Average them over the time length
-        A_n = (2 / #t) * A_n
-        B_n = (2 / #t) * B_n
+        cosC = (2 / #t) * cosC
+        sinC = (2 / #t) * sinC
 
         -- Store the coefficients
-        coeffs[n] = { A = A_n, B = B_n }
+        coeffs[n] = { cos = cosC, sin = sinC }
     end
 
     return coeffs
@@ -70,8 +68,9 @@ function Fourier:transformSawtooth(i, f, t)
     local term = 0
 
     for n = 1, i do
-        term = term + (-1 ^ n * math.sin(2 * Math:pi(17) * n * f * t) / n)
+        term = term + ((-1) ^ n * math.sin(2 * Math:pi() * n * f * t) / n)
     end
     return 2 / Math:pi() * term
 end
+
 return Fourier
--- fourier.lua
+++ fourier.lua
@@ -1,27 +1,25 @@
 ---@diagnostic disable: need-check-nil
 local Fourier = require "Fourier"
 
--- Example usage
 local signal = {}
-local t = {}
-local harmonic = 3
+local time = {}
+local harmonic = 30
 
 
--- Generate a simple sine wave for the example
-local f = 1
-for i = 1, 50 do
-    t[i] = (i - 1) * 0.01  -- time array (0.01 second intervals)
-    signal[i] = math.sin(2 * math.pi * f * t[i])
+local frequency = 1
+for i = 1, 1000 do
+    time[i] = (i - 1) * 0.001  -- time array (0.001 second intervals)
+    signal[i] = Fourier:transformSawtooth(i, frequency, time[i])
 end
 
 -- Decompose the signal into Fourier coefficients
-local coeffs = Fourier:decompose(signal, harmonic, f, t)
+local coeffs = Fourier:decompose(signal, harmonic, frequency, time)
 
 local file = io.open("wave.dat", "w")
 
 -- Recompose the signal from the coefficients
 local recomposeSignal = {}
-for i, ti in ipairs(t) do
-    recomposeSignal[i] = Fourier:recompose(coeffs, f, ti)
+for i, ti in ipairs(time) do
+    recomposeSignal[i] = Fourier:recompose(coeffs, frequency, ti)
     file:write(string.format("%f\t %f\t %f\n", ti, signal[i], recomposeSignal[i]))
 end

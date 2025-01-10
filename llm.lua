3822b101645e244613b2880e9f6b6b5d04422ad2
Cvar1984 <cvar1984@cvar1984.my.id>
2025-01-10 15:15:45 +0700

    disable other model

--- llm.lua
+++ llm.lua
@@ -1,7 +1,7 @@
 local LLMClient = require "LLMClient"
 
 local qwen = LLMClient:new("qwen2.5:0.5b")
-qwen.temp = 1
+qwen.temp = 0.1
 
 local mathstral = LLMClient:new("mathstral:latest")
 mathstral.temp = 0.3
@@ -13,5 +13,5 @@ local prompt = io.read()
 local rawRensponse = qwen:makeRequest(prompt)
 print(qwen:parseResponse(rawRensponse))
 
-local rawRensponse = mathstral:makeRequest(prompt)
-print(mathstral:parseResponse(rawRensponse))
+--rawRensponse = mathstral:makeRequest(prompt)
+--print(mathstral:parseResponse(rawRensponse))
